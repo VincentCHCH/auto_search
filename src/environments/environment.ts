40101f18183d4905ebd065bb6883663dfc8e4c11
Chen, Hao (Vincent) [MEDCN] <HChen180@its.jnj.com>
2018-11-16 14:35:47 +0800

    update network

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: `./`,
+  // SERVER_URL: `./`,
+
+  SERVER_URL: `https://41c0ede7-bac0-49e6-a786-ea1a8952d855.mock.pstmn.io/`,
   production: false,
   useHash: true,
   hmr: false,
